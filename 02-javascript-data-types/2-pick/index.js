022c63354f49792bc0ed0458d8a5c427a7308534
KirillKoch <kochergin2231@work@gmail.com>
2022-08-04 14:49:44 +0300

    solving 3 tasks from 2 modules of the course book

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,13 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-
+  return Object.fromEntries(Object.entries(obj).filter(([key]) => fields.includes(key)));
 };
+
+const fruits = {
+  apple: 2,
+  orange: 4,
+  banana: 3
+};
+
+pick(fruits, 'apple', 'banana');
--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,13 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+  return Object.fromEntries(Object.entries(obj).filter(([key]) => !fields.includes(key)));
 };
+
+const fruits = {
+  apple: 2,
+  orange: 4,
+  banana: 3
+};
+
+omit(fruits, "apple", "banana");
