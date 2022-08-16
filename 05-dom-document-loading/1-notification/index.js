79c28844058f25babf907b8a2a7bcec3d5f2ff2d
KirillKoch <kochergin2231@work@gmail.com>
2022-08-16 15:20:19 +0300

    solving 2 tasks from 5 modules of the course book

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,62 @@
 export default class NotificationMessage {
+  constructor(
+    message = '',
+    {
+      duration = 1000,
+      type = ''
+    } = {}) {
 
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+
+    this.render();
+  }
+
+  getTemplate() {
+    return `
+        <div class="notification ${this.type}" style="--value:${this.millisecondsToSeconds(this.duration)}s" data-element="notification">
+            <div class="timer"></div>
+            <div class="inner-wrapper">
+                <div class="notification-header">${this.type}</div>
+                <div class="notification-body">
+                    ${this.message}
+                </div>
+            </div>
+        </div>
+        `;
+  }
+
+  render(elem) {
+    const element = document.createElement('div');
+    element.innerHTML = this.getTemplate();
+    this.element = element.firstElementChild;
+
+    document.body.append(this.element);
+  }
+
+  millisecondsToSeconds(milliseconds) {
+    return milliseconds / 1000;
+  }
+
+  show() {
+    const notificationElements = document.querySelectorAll('[data-element="notification"]');
+
+    if (notificationElements.length > 1) {
+      notificationElements[0].remove();
+    }
+
+    setTimeout(() => this.destroy(), this.duration);
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+  }
 }
--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,6 +1,88 @@
+import {sortStrings} from '../../02-javascript-data-types/1-sort-strings/index.js';
+
 export default class SortableTable {
   constructor(headerConfig = [], data = []) {
+    this.headerConfig = headerConfig;
+    this.data = data;
 
+    this.render();
+  }
+
+  getTemplate() {
+    return `
+    <div data-element="productsContainer" class="products-list__container">
+      <div class="sortable-table">
+
+        <div data-element="header" class="sortable-table__header sortable-table__row">
+          ${this.addCellTableHeader(this.headerConfig)}
+        </div>
+
+        <div data-element="body" class="sortable-table__body">
+          ${this.addCellTableBody(this.headerConfig, this.data)}
+        </div>
+
+        <div data-element="loading" class="loading-line sortable-table__loading-line"></div>
+
+        <div data-element="emptyPlaceholder" class="sortable-table__empty-placeholder">
+          <div>
+            <p>No products satisfies your filter criteria</p>
+            <button type="button" class="button-primary-outline">Reset all filters</button>
+          </div>
+        </div>
+      </div>
+        `;
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = this.getTemplate();
+    this.element = element.firstElementChild;
+  }
+
+  addCellTableHeader(config) {
+    return config.map(elem => {
+      return `
+        <div class="sortable-table__cell" data-id="${elem.id}" data-sortable="${elem.sortable}">
+          <span>${elem.title}</span>
+        </div>
+      `;
+    })
+    .join('');
+  }
+
+  addCellTableBody(config, data) {
+    const template = `<a href="#" class="sortable-table__row">`;
+
+    return data.map(dataElem => {
+      return template + config.map(configElem => {
+        if (configElem.id === 'images') {
+          return configElem.template(dataElem[configElem.id]);
+        }
+
+        return `
+          <div class="sortable-table__cell">${dataElem[configElem.id]}</div>
+        `;
+      }).join('') + '</a>';
+    })
+    .join('');
+  }
+
+  sort(field, order) {
+    const arr = this.element.querySelectorAll('.sortable-table__row');
+
+    sortStrings(arr, 'asc');
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
   }
 }
+
 
